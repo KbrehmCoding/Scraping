63e096c5f72a63807546d18081a88b3c651452e6
Krista Brehm <kristabrehm1@gmail.com>
2019-02-13 11:33:19 -0500

    set up the files in the models folder

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var exphb = require("express-handlebars");
 var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+
 
 var db = require("./models");
 
